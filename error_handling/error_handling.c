c7d21f4639a6dfd28130e9100346ab58c81e8ada
maximarquezregis <maximarquez2004@gmail.com>
2025-09-15 11:02:12 -0300

    added error_handling methods

--- error_handling/error_handling.c
+++ error_handling/error_handling.c
@@ -2,20 +2,39 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
 
 void error_parse(int line, char* msg) {
-    fprintf(stderr, "Parse error: %s in line %d\n", msg, line);
+    fprintf(stderr, "Parse error(line %d): %s \n", line, msg);
     exit(EXIT_FAILURE);
 }
 
 void error_lexical(int line, char* msg) {
-    fprintf(stderr, "Lexical ERROR(line %d): unknown character '%s'\n", line, msg); 
+    fprintf(stderr, "Lexical error(line %d): unknown character '%s'\n", line, msg);
     exit(EXIT_FAILURE); 
 }
 
 int error_open_file(char* filename) {
     fprintf(stderr, "Error: could not open file %s\n", filename);
     return 1;
+}
+
+void error_variable_redeclaration(int line, char* msg) {
+    fprintf(stderr, "Error(line %d): variable %s redeclared \n", line, msg);
+    exit(EXIT_FAILURE);
+}
+
+void error_variable_not_declared(int line, char* msg) {
+    fprintf(stderr, "Error(line %d): variable %s not declared \n", line, msg);
+    exit(EXIT_FAILURE);
+}
+
+void error_type_mismatch(int line, char* msg, char* type) {
+    fprintf(stderr, "Error(line %d): conflicting types for variable %s (should be %s) \n", line, msg, type);
+    exit(EXIT_FAILURE);
+}
+
+void error_allocate_mem() {
+    fprintf(stderr, "Error: Failed to allocate memory \n");
+    exit(EXIT_FAILURE);
 }
--- error_handling/error_handling.h
+++ error_handling/error_handling.h
@@ -4,5 +4,9 @@
 void error_parse(int line, char* msg);
 void error_lexical(int line, char* msg);
 int error_open_file(char* filename);
+void error_variable_redeclaration(int line, char* msg);
+void error_variable_not_declared(int line, char* msg);
+void error_type_mismatch(int line, char* msg, char* type);
+void error_allocate_mem();
 
 #endif
