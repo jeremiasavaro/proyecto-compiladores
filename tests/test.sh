9c5f60eb24b0fc4457f873b02f8830a7876aba96
maximarquezregis <maximarquez2004@gmail.com>
2025-10-01 20:46:14 -0300

    added test counter to tests.sh script and now it runs all tests

--- tests/test.sh
+++ tests/test.sh
@@ -7,11 +7,16 @@ if [ ! -x ./parser ]; then
     exit 1
 fi
 
+i=0
+
 # Recorre todos los archivos en tests
-for file in tests/*; do
+for file in tests/*/*; do
     if [ -f "$file" ] && [ "$file" != "tests/test.sh" ]; then
+        ((i++))   # incrementa i en bash
         echo ">>> Ejecutando ./parser $file"
         ./parser "$file"
         echo "-----------------------------------"
     fi
 done
+
+echo "Tests ejecutados: $i"
