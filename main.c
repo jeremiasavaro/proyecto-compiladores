12f98bd75919646a38d169a693c025fb3a3df84a
Mateo Cornejo <ccornejomateo@gmail.com>
2025-11-06 20:00:00 -0300

    First aproach of the flags

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include "intermediate_code.h"
 #include "symbol.h"
 #include "object_code.h"
+#include <ctype.h>
 
 extern int yylineno;
 extern int yyparse();
@@ -53,6 +54,7 @@ int main(int argc, char *argv[]) {
 			}
 		} else if (strcmp(argv[i], "-target") == 0) {
 			if (i + 1 < argc) { // If -target is specified, a target stage after that is required
+				str_to_lower(argv[i + 1]);
 				if (strcmp(argv[i + 1], "scan") == 0) {
 					stage = SCAN;
 				} else if (strcmp(argv[i + 1], "parse") == 0) {
@@ -90,24 +92,48 @@ int main(int argc, char *argv[]) {
 	}
 	yyin = file;
 
-	// TODO: use flags to decide what to do next
+	if (stage == 0) {
+		printf("=== SYNTAX ANALYSIS ===\n");
+		while (yylex() != 0) {
+		}		
+		printf("No syntactic errors.\n");
+	}
 
-	printf("=== SYNTAX ANALYSIS ===\n");
-	yyparse();
-	print_full_ast(head_ast);
-	print_symbol_table(global_level);
-	semantic_analyzer(head_ast);
+	if (stage > 0) {
+		printf("=== SYNTAX AND SEMANTIC ANALYSIS ===\n");
+		yyparse();
+		semantic_analyzer(head_ast);
+	}
 	// Generate intermediate code for each top-level method declaration
-	reset_code();
-	for (AST_ROOT* cur = head_ast; cur != NULL; cur = cur->next) {
-		gen_code(cur->sentence, NULL);
+	if (stage > 1) {
+		reset_code();
+		for (AST_ROOT* cur = head_ast; cur != NULL; cur = cur->next) {
+			gen_code(cur->sentence, NULL);
+		}
+		print_code_to_file("intermediate_code/intermediate_code.out");
 	}
-	print_code_to_file("intermediate_code/intermediate_code.out");
-	FILE* out = fopen("res.out", "w");
-	if (!out) {
-		error_open_file("res.out");
+	if (stage > 2) { 
+		FILE* out = fopen(outname, "w");
+		if (!out) {
+			error_open_file(outname);
+		}
+		generate_object_code(out);
+		fclose(out);
 	}
-	generate_object_code(out);
-	fclose(out);
+	if (stage > 3) {
+		system("./run_executable.sh");
+	}
+	if (debug) {
+		if (head_ast != NULL && global_level != NULL) {
+			print_full_ast(head_ast);
+			print_symbol_table(global_level);
+		}
+	}
 	return 0;
+}
+
+void str_to_lower(char *s) {
+    for (int i = 0; s[i]; i++) {
+        s[i] = tolower((unsigned char)s[i]);
+    }
 }
