88d1fc5d232b4c08d2e0886a57751d1bc7e9b800
maximarquezregis <maximarquez2004@gmail.com>
2025-11-08 15:36:06 -0300

    rollback of optimization in object_code because now is not needed

--- object_code/object_code.h
+++ object_code/object_code.h
@@ -9,12 +9,19 @@
 #include "symbol.h"
 #include "ast.h"
 
+#define MAX_VARS_PER_FUNCTION 200
+
 // Structure for saving variable name and offset
 typedef struct {
     char* name;
     int offset;
 } VarLocation;
 
-void generate_object_code(FILE* out_file, cant_ap_temp* cant_ap_h);
+/* Main function to generate x86-64 assembly code from intermediate code
+ * Outputs the assembly code to the provided file pointer
+ * Handles function prologues/epilogues, arithmetic operations, control flow, and function calls
+ * Uses a simple mapping of temporaries to stack offsets
+ */
+void generate_object_code(FILE* out_file, CANT_AP_TEMP* cant_ap_h);
 
 #endif
